212c93aa46bda30d2745ca44db46757786841304
Waris Akinocho <warisakinocho3@gmail.com>
2025-03-24 14:31:34 +0100

    refactor

--- src/tests/sum.test.js
+++ src/tests/sum.test.js
@@ -1,5 +1,7 @@
-test("object assignment", () => {
-	var data = { one: 1 };
-	data["two"] = 2;
-	expect(data).toEqual({ one: 1, two: 2 });
+describe("object assignment", () => {
+	it("should check if an object match { one: 1, two: 2 }", () => {
+		var data = { one: 1 };
+		data["two"] = 2;
+		expect(data).toEqual({ one: 1, two: 2 });
+	});
 });

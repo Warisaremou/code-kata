e6ca8702842616c4aa3d40ccfe5f8d803e0a8553
Waris Akinocho <warisakinocho3@gmail.com>
2025-03-29 11:29:12 +0100

    Refactor function

--- src/functions/numbers-sum.js
+++ src/functions/numbers-sum.js
@@ -1,11 +1,9 @@
 function getSum(a, b) {
-	const maxValue = Math.max(a, b);
-	const minValue = Math.min(a, b);
-
-	return Array.from({ length: maxValue - minValue + 1 }, (_, index) => minValue + index).reduce(
-		(acc, value) => acc + value,
-		0
-	);
+	if (a > b) [b, a] = [a, b];
+	// Method 1 using Array.from() and reduce()
+	// return Array.from({ length: b - a + 1 }, (_, index) => a + index).reduce((acc, value) => acc + value, 0);
+	// Method 2 using formula for the sum of the terms of an arithmetic sequence: S = number_of_terms * (a + b) / 2
+	return ((b - a + 1) * (a + b)) / 2;
 }
 
 export { getSum };
